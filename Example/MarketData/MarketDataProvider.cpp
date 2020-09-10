daa671e87d302a51c2119cf23bd0703902612d5d
guido <knowact@outlook.com>
2020-09-10 17:15:36 +0800

    004

--- Example/MarketData/MarketDataProvider.cpp
+++ Example/MarketData/MarketDataProvider.cpp
@@ -3,8 +3,12 @@
 #include <chrono>
 #include <iostream>
 
+
 #include "MarketData/MarketData.h"
 #include "UsualHeader.h"
+
+
+#define LOG_RSP_INFO LOGD("ReqID {} IsLast {} ErrorID {} ErrorMsg {} ", nRequestID, bIsLast, pRspInfo ? pRspInfo->ErrorID : 0, pRspInfo ? pRspInfo->ErrorMsg : "null")
 
 MarketDataProvider::MarketDataProvider() :
     _subsciber(nullptr),
@@ -19,8 +23,9 @@ void MarketDataProvider::run() {
 
 void MarketDataProvider::doRun() { 
     LOGD("");
-    _timer.expires_after(std::chrono::seconds(2));
-    _timer.async_wait(std::bind(&MarketDataProvider::onTimerExpire, this, std::placeholders::_1));
+    //_timer.expires_after(std::chrono::seconds(2));
+    //_timer.async_wait(std::bind(&MarketDataProvider::onTimerExpire, this, std::placeholders::_1));
+    setupCTPMdApi();
     _ioctx.run();    
 }
 
@@ -37,5 +42,101 @@ void MarketDataProvider::publishMarketData(const boost::shared_ptr<MarketData> &
     if (_subsciber) {
         _subsciber->onMarketDataUpdate(market_data); 
     }
+}
+
+void MarketDataProvider::setupCTPMdApi() {
+    LOGD("");
+    _ctp_md_api = CThostFtdcMdApi::CreateFtdcMdApi("./flow_md/");
+    _ctp_md_api->RegisterSpi(this);
+    const char *front_addr { "tcp://180.168.146.187:10110" };
+    _ctp_md_api->RegisterFront(const_cast<char*>(front_addr));
+    _ctp_md_api->Init();
+}
+
+void MarketDataProvider::OnFrontConnected() {
+    LOGD("");
+    CThostFtdcReqUserLoginField req = {0};
+    int ec = _ctp_md_api->ReqUserLogin(&req, genReqID());
+    if (ec != 0) {
+        LOGW("ctp md ReqUserLogin return {}", ec);
+    }
+}
+
+void MarketDataProvider::OnFrontDisconnected(int nReason) {
+    LOGD("");
+}
+
+void MarketDataProvider::OnHeartBeatWarning(int nTimeLapse) {
+    LOGD("");
+
+}
+
+void MarketDataProvider::OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin,
+                                        CThostFtdcRspInfoField *pRspInfo,
+                                        int nRequestID, bool bIsLast) {
+    //LOGD("ReqID {} IsLast {} ErrorID {} ErrorMsg {} ", nRequestID, bIsLast, pRspInfo ? pRspInfo->ErrorID : 0, pRspInfo ? pRspInfo->ErrorMsg : "nullptr");
+    LOG_RSP_INFO;
+
+    const char * instrument_list[]{"IF2009", };
+    _ctp_md_api->SubscribeMarketData(const_cast<char**>(instrument_list), 1);
+}
+
+///登出请求响应
+void MarketDataProvider::OnRspUserLogout(CThostFtdcUserLogoutField *pUserLogout, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) {
+    LOGD("");
+
+}
+
+///错误应答
+void MarketDataProvider::OnRspError(CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) {
+    LOG_RSP_INFO;
+
+}
+
+///订阅行情应答
+void MarketDataProvider::OnRspSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) {
+    LOG_RSP_INFO;
+    if (pSpecificInstrument) {
+        LOGD("InstrumntID {}", pSpecificInstrument->InstrumentID);
+    } else {
+        LOGD("pSpecificInstrument is null"); 
+    }
+}
+
+///取消订阅行情应答
+void MarketDataProvider::OnRspUnSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) {
+    LOGD("");
+
+}
+
+///订阅询价应答
+void MarketDataProvider::OnRspSubForQuoteRsp(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) {
+    LOGD("");
+
+}
+
+///取消订阅询价应答
+void MarketDataProvider::OnRspUnSubForQuoteRsp(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) {
+    LOGD("");
+
+}
+
+///深度行情通知
+void MarketDataProvider::OnRtnDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarketData) {
+    LOGD("");
+
+}
+
+///询价通知
+void MarketDataProvider::OnRtnForQuoteRsp(CThostFtdcForQuoteRspField *pForQuoteRsp) {
+    LOGD("");
+
+}
+
+MarketDataProvider::~MarketDataProvider() {
+    _subsciber = nullptr;
+    _ctp_md_api->Release();
+    //_ctp_md_api->Join(); //_ctp_md_api usable after release?
+    _ctp_md_api = nullptr;
 }
 
--- Example/MarketData/MarketDataProvider.h
+++ Example/MarketData/MarketDataProvider.h
@@ -4,6 +4,8 @@
 #include <boost/smart_ptr.hpp>
 #include <boost/asio.hpp>
 
+#include <ctp/ThostFtdcMdApi.h>
+
 class MarketData;
 
 class MarketDataSubscriber {
@@ -11,9 +13,10 @@ public:
     virtual void onMarketDataUpdate(const boost::shared_ptr<MarketData> &mark_data) = 0;
 };
 
-class MarketDataProvider {
+class MarketDataProvider : public CThostFtdcMdSpi {
 public:
     MarketDataProvider();
+    ~MarketDataProvider();
 
     void setMarketDataSubscriber(MarketDataSubscriber *subsciber) {
         _subsciber = subsciber;
@@ -22,15 +25,61 @@ public:
     void run();
 
 private:
+    void setupCTPMdApi();
+    //CThostFtdcMdSpi 
+    virtual void OnFrontConnected() override;
+    virtual void OnFrontDisconnected(int nReason) override;
+
+    ///心跳超时警告。当长时间未收到报文时，该方法被调用。
+	///@param nTimeLapse 距离上次接收报文的时间
+	virtual void OnHeartBeatWarning(int nTimeLapse) override;
+
+    virtual void OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin,
+                                CThostFtdcRspInfoField *pRspInfo,
+                                int nRequestID, bool bIsLast) override;
+    ///登出请求响应
+	virtual void OnRspUserLogout(CThostFtdcUserLogoutField *pUserLogout, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) override;
+
+	///错误应答
+	virtual void OnRspError(CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) override;
+
+	///订阅行情应答
+	virtual void OnRspSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) override;
+
+	///取消订阅行情应答
+	virtual void OnRspUnSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) override;
+
+	///订阅询价应答
+	virtual void OnRspSubForQuoteRsp(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) override;
+
+	///取消订阅询价应答
+	virtual void OnRspUnSubForQuoteRsp(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) override;
+
+	///深度行情通知
+	virtual void OnRtnDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarketData) override;
+
+	///询价通知
+	virtual void OnRtnForQuoteRsp(CThostFtdcForQuoteRspField *pForQuoteRsp) override;
+
+
+private:
     void doRun();
     void onTimerExpire(const boost::system::error_code &error);
     void publishMarketData(const boost::shared_ptr<MarketData> &market_data);
+    //Generate Request ID
+    int genReqID() {
+        return ++_md_req_id; 
+    }
+
 
 private:
-    MarketDataSubscriber *_subsciber;
+    MarketDataSubscriber *_subsciber{nullptr};
     boost::asio::io_context _ioctx;
     boost::asio::executor_work_guard<boost::asio::io_context::executor_type> _work_guard;
     boost::asio::steady_timer _timer;
+
+    CThostFtdcMdApi *_ctp_md_api{nullptr};
+    int _md_req_id{0};
 };
 
 #endif //KA_MARKET_DATA_PROVIDER_H_
